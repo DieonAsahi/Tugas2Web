6b5053610ef354910fe3f115d36669ca5a09f736
DieonAsahi <athawaguth@gmail.com>
2025-03-23 23:12:55 +0700

    [Pertemuan 3] Membuat Layout dan Component

---
+++ resources/views/web/homepage.blade.php
@@ -0,0 +1,9 @@
+  <x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <h3>Ini adalah halaman Homepage</h3>
+
+    <x-card></x-card>
+
+    <x-alert></x-alert>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,46 @@ Route::get('/helloworld', function() {
 });
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    $title = "Homepage";
+
+    return view('web.homepage', ['title' =>$title]);
+});#->name('home');
+
+Route::get('products', function () {
+    $title = "Products";
+
+    return view('web.products', ['title' =>$title]);
+});
+
+Route::get('product/{slug}', function ($slug) {
+    $title = "Single Products";
+
+    return view('web.single_product', ['title' =>$title, 'slug'=>$slug]);
+});
+
+Route::get('categories', function () {
+    $title = "Categories";
+
+    return view('web.categories', ['title' =>$title]);
+});
+
+Route::get('category/{slug}', function ($slug) {
+    $title = "Single Category";
+
+    return view('web.single_category', ['title' =>$title, 'slug'=>$slug]);
+});
+
+Route::get('cart', function () {
+    $title = "Cart";
+
+    return view('web.cart', ['title' =>$title]);
+});
+
+Route::get('checkout', function () {
+    $title = "Checkout";
+
+    return view('web.checkout', ['title' =>$title]);
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
