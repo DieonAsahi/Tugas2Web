bccb75d5bcf967be4d211c29720ce7e911b75a10
DieonAsahi <149960551+DieonAsahi@users.noreply.github.com>
2025-06-20 16:46:18 +0700

    Update 2025_04_17_065422_create_product_categories_table--create=product_categories.php

--- database/migrations/2025_04_17_065422_create_product_categories_table--create=product_categories.php
+++ database/migrations/2025_04_17_065422_create_product_categories_table--create=product_categories.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_categories', function (Blueprint $table) { 
-            $table->id(); 
-            $table->string('name'); 
-            $table->string('slug')->unique(); 
-            $table->text('description')->nullable(); 
-            $table->string('image')->nullable(); 
-            $table->timestamps(); 
-            }); 
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
